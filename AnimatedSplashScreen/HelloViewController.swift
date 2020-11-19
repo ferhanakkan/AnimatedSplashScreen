91532c6963bd8c4374eb58b8b6a9d2aa86bf762b
Ferhan Akkan <>
2020-11-19 17:45:21 +0300

    Demo app completed

---
+++ AnimatedSplashScreen/HelloViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HelloViewController.swift
+//  AnimatedSplashScreen
+//
+//  Created by Ferhan Akkan on 19.11.2020.
+//
+
+import UIKit
+
+class HelloViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- AnimatedSplashScreen/ViewController.swift
+++ AnimatedSplashScreen/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import SwiftyGif
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SwiftyGifDelegate  {
 
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var appcentLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        logoImageView.delegate = self
+
+        do {
+            let gif = try UIImage(gifName: "logoGif.gif")
+            self.logoImageView.setGifImage(gif, loopCount: 1)
+        } catch {
+            print(error)
+        }
+        view.layoutIfNeeded()
+    }
+    
+    func setLabelsHiddenAnimation() {
+        UIView.animate(withDuration: 1.0) {
+            self.appcentLabel.alpha = 0
+        } completion: { (_) in
+            self.setLabelsShowAnimation()
+        }
+    }
+    
+    func setLabelsShowAnimation() {
+        UIView.animate(withDuration: 1.0) {
+            self.appcentLabel.alpha = 1
+        } completion: { (_) in
+            self.setLabelsHiddenAnimation()
+        }
     }
 
+    func gifDidStop(sender: UIImageView) {
+        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "HelloVC")
+        self.view.window?.rootViewController = vc
+    }
 
 }
 
